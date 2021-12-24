c1856f92ba3ed8b8325dc012ad1f2f81b855049f
liujiajun <liujiajun@ecqun.com>
2021-12-24 09:52:46 +0800

    代码分割小优化

--- webpack/constant.js
+++ webpack/constant.js
@@ -6,6 +6,9 @@ const PROJECT_NAME = path.parse(PROJECT_PATH).name
 const SERVER_HOST = '127.0.0.1'
 const SERVER_PORT = 9000
 
+const ANALYZER_HOST = '127.0.0.1'
+const ANALYZER_PORT = 8888
+
 const isDev = process.env.NODE_ENV !== 'production'
 const isProd = process.env.NODE_ENV === 'production'
 
@@ -16,4 +19,6 @@ module.exports = {
   isProd,
   SERVER_HOST,
   SERVER_PORT,
+  ANALYZER_HOST,
+  ANALYZER_PORT,
 }
--- webpack/webpack.prod.conf.js
+++ webpack/webpack.prod.conf.js
@@ -2,6 +2,7 @@ const { merge } = require('webpack-merge')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin') // 清理原来的打包文件
 const CssMinimizerWebpackPlugin = require('css-minimizer-webpack-plugin')
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
+const { ANALYZER_HOST, ANALYZER_PORT } = require('./constant')
 const { baseConfig, resolvePath } = require('./webpack.base.conf')
 
 module.exports = merge(baseConfig, {
@@ -9,21 +10,22 @@ module.exports = merge(baseConfig, {
   devtool: false,
   output: {
     path: resolvePath('../dist'),
-    filename: '[name].[contenthash:8].bundle.js',
+    filename: 'js/[name].[contenthash:8].bundle.js',
   },
   plugins: [
     new CleanWebpackPlugin(),
     new BundleAnalyzerPlugin({
       analyzerMode: 'server', // 开一个本地服务查看报告
-      analyzerHost: '127.0.0.1', // host 设置
-      analyzerPort: 8888, // 端口号设置
+      analyzerHost: ANALYZER_HOST, // host 设置
+      analyzerPort: ANALYZER_PORT, // 端口号设置
     }),
   ],
   optimization: {
     minimize: true,
     minimizer: [new CssMinimizerWebpackPlugin()],
     splitChunks: {
-      chunks: 'async',
+      chunks: 'all',
+      minSize: 0,
       name(module, chunks, cacheGroupKey) {
         const moduleFileName = module
           .identifier()
@@ -31,24 +33,6 @@ module.exports = merge(baseConfig, {
           .reduceRight((item) => item)
         const allChunksNames = chunks.map((item) => item.name).join('~')
         return `${cacheGroupKey}-${allChunksNames}-${moduleFileName}`
-      },
-      minSize: 20000,
-      minRemainingSize: 0,
-      minChunks: 1,
-      maxAsyncRequests: 30,
-      maxInitialRequests: 30,
-      enforceSizeThreshold: 50000,
-      cacheGroups: {
-        vendors: {
-          test: /[\\/]node_modules[\\/]/,
-          priority: -10,
-          reuseExistingChunk: true,
-        },
-        default: {
-          minChunks: 2,
-          priority: -20,
-          reuseExistingChunk: true,
-        },
       },
     },
   },
